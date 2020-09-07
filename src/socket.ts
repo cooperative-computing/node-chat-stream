eaecd7c4bd6b26c34d18cd007f79271b68286c47
Muhammad Qasim <muhammad.qasim@cooperativecomputing.com>
2020-09-07 17:22:52 +0500

    Implemented chat room join/leave user2user chat.

--- src/socket.ts
+++ src/socket.ts
@@ -1,20 +1,15 @@
 //Models
 import ChatList from './Models/ChatList';
-
-
 import Helper from './Helper';
-
-
-
 
 const Socket_IO = (socket) => {
   var clients: any[] = [];
+  var roomData: any[] = [];
   //setup event listener
   socket.on("connection", async client => {
 
     client.on("node-chat-join", async e => {
       let user_id = await Helper.getUserId(e);
-      console.log("node-chat-join ", user_id);
 
       if (!user_id) return false;
       client.user_id = user_id;
@@ -33,12 +28,76 @@ const Socket_IO = (socket) => {
           targetClients.splice(i, 1);
         }
       }
+      // loop through every room(chat list)
+      for (let i in roomData) {
+        // check if room(chat_list_id) is not defined and not empty
+        if (roomData[i] !== undefined && roomData[i].length > 0) {
+          // retrieve the index against disconnecting userId in roomData(Object) roomId(Array)
+          const ridx = roomData[i].indexOf(client.user_id);
+          if (ridx > -1) {
+            // remove index/instance from roomData(Object) roomId(Array)
+            roomData[i].splice(ridx, 1);
+          }
+        }
+      }
+    });
+
+    // catch on user joins the chat room
+    client.on("join_room", async room => {
+      client.join('user-to-user-chat-room');
+      // check if roomId(chat_list_id) exists in roomData(Object)
+      if (room.roomId in roomData) {
+        // check if userId exists in roomData(Object) in roomId(Array)
+        if (roomData[room.roomId].indexOf(room.userId) < 0) {
+          // add userId in roomId(Array)
+          roomData[room.roomId].push(room.userId);
+        }
+      } else {
+        // check if roomId(chat_list_id) is defined and empty
+        if (roomData[room.roomId] === undefined || roomData[room.roomId].length == 0) {
+          // create empty Array against new room(chat_list_id)
+          roomData[room.roomId] = [];
+          // add userId in roomId(Array)
+          roomData[room.roomId].push(room.userId);
+        } else {
+          // check if userId exists in roomData(Object) in roomId(Array)
+          if (roomData[room.roomId].indexOf(room.userId) < 0) {
+            // add userId in roomId(Array)
+            roomData[room.roomId].push(room.userId);
+          }
+        }
+      }
+    });
+
+    // catch on user leaves the chat room
+    client.on("leave_room", async room => {
+      client.leave('user-to-user-chat-room');
+      // check if roomId(chat_list_id) exists in roomData(Object)
+      if (room.roomId in roomData) {
+        // check if roomId has sender/receiver(user ids[Array])
+        if (roomData[room.roomId].length > 0) {
+          // retrieve the index against userId in roomData(Object) roomId(Array)
+          const idx = roomData[room.roomId].indexOf(room.userId);
+          if (idx > -1) {
+            // remove index/instance from roomData(Object) roomId(Array)
+            roomData[room.roomId].splice(idx, 1);
+          }
+        }
+      }
     });
 
     // user to user start
     client.on("user-message", async event => {
       let ids = [event.receiver];
       if (event.include_sender) ids.push(event.sender);
+      // check room exists against chat list id
+      if (event.chat_list_id in roomData) {
+        // check if user on other side is not active or joined chat room against chat list id
+        if (roomData[event.chat_list_id].indexOf(event.receiver) < 0) {
+          // send chat notification event on client side
+          client.emit('send-chat-notification', true);
+        }
+      }
       ids.forEach((id: any) => {
         if (clients[id] && clients[id].length > 0) {
           clients[id].forEach(cli => {
@@ -68,6 +127,7 @@ const Socket_IO = (socket) => {
         Helper.sendMultiUserMsg(room, clients, data);
         Helper.addChat(data); //save chat to the database
       }
+      client.emit('send-group-message', data);
     });
     // user to group end
 
