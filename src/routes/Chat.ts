4349def702289cb0b553dcd2fb24fd73a5a9e08b
sbilal269 <122864785+sbilal269@users.noreply.github.com>
2023-01-17 15:26:43 +0500

    CW-45 #time 9h #comment [Chat, ChatList, ChatHistory] | [APIs] | [Delete chat functionality]

--- src/routes/Chat.ts
+++ src/routes/Chat.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import ChatList from '../Models/ChatList';
-import Chat from '../Models/Chat';
-import Helper from '../Helper';
+import express from "express";
+import ChatList from "../Models/ChatList";
+import Chat from "../Models/Chat";
+import ChatHistory from "../Models/ChatHistory";
+import Helper from "../Helper";
 
 const ChatRoutes = express.Router();
 
@@ -16,35 +17,60 @@ ChatRoutes.route("/").get(async (req, res, next) => {
   let created_by = param.created_by;
   let name = param.name;
   let users_info_Url = param.users_info_Url;
+  let userId = param.userId;
 
   try {
     if (chat_list_id && users_info_Url) {
-      query = { chat_list_id };
+      const historyExist = await ChatHistory.findOne({ userId, chat_list_id });
+      if (historyExist) {
+        query = { chat_list_id, updatedAt: { $gte: historyExist.updatedAt } };
+      } else {
+        query = { chat_list_id };
+      }
+
       let get_chatList = await ChatList.findOne({ _id: chat_list_id });
-      let paginationData = await Chat.paginate(query, { page, limit, sort: { createdAt: -1 }, lean: true });
-      let getUserDetails = await Helper.getUserDetails(users_info_Url, get_chatList.receivers);
-      paginationData.docs = Helper.chatAddUsers(paginationData.docs, getUserDetails);
+      let paginationData = await Chat.paginate(query, {
+        page,
+        limit,
+        sort: { createdAt: -1 },
+        lean: true,
+      });
+      let getUserDetails = await Helper.getUserDetails(
+        users_info_Url,
+        get_chatList.receivers
+      );
+      paginationData.docs = Helper.chatAddUsers(
+        paginationData.docs,
+        getUserDetails
+      );
       Helper.sendPaginationResponse(res, paginationData);
-    }
-    else if (chat_type && created_by) {
+    } else if (chat_type && created_by) {
       //fetch chat for group/chatlist if chat_list_id not found
       let query: any = { chat_type, created_by };
       if (name) query.name = name;
       let get_chatList = await ChatList.findOne(query);
       chat_list_id = get_chatList ? get_chatList._id : false;
       if (chat_list_id && users_info_Url) {
-        let getUserDetails = await Helper.getUserDetails(users_info_Url, get_chatList.receivers);
-        let paginationData = await Chat.paginate({ chat_list_id }, { page, limit, sort: { createdAt: -1 }, lean: true });
-        paginationData.docs = Helper.chatAddUsers(paginationData.docs, getUserDetails);
-        return Helper.sendPaginationResponse(res, paginationData, { chat_list_id });
+        let getUserDetails = await Helper.getUserDetails(
+          users_info_Url,
+          get_chatList.receivers
+        );
+        let paginationData = await Chat.paginate(
+          { chat_list_id },
+          { page, limit, sort: { createdAt: -1 }, lean: true }
+        );
+        paginationData.docs = Helper.chatAddUsers(
+          paginationData.docs,
+          getUserDetails
+        );
+        return Helper.sendPaginationResponse(res, paginationData, {
+          chat_list_id,
+        });
       }
-      Helper.sendNotFoundResponse(res, 'Group/chatlist not found.');
-
-    }
-    else Helper.sendNotFoundResponse(res, 'message');
-  }
-  catch (error) {
-    Helper.errorResponse(res, 'Something went wrong.Please try again.');
+      Helper.sendNotFoundResponse(res, "Group/chatlist not found.");
+    } else Helper.sendNotFoundResponse(res, "message");
+  } catch (error) {
+    Helper.errorResponse(res, "Something went wrong.Please try again.");
   }
 });
 
--- src/routes/ChatList.ts
+++ src/routes/ChatList.ts
@@ -1,7 +1,8 @@
-import express from 'express';
-import ChatList from '../Models/ChatList';
-import Chat from '../Models/Chat';
-import Helper from '../Helper';
+import express from "express";
+import ChatList from "../Models/ChatList";
+import ChatHistory from "../Models/ChatHistory";
+import Chat from "../Models/Chat";
+import Helper from "../Helper";
 
 const ChatListRoutes = express.Router();
 
@@ -17,21 +18,31 @@ ChatListRoutes.route("/").get(async (req, res, next) => {
     try {
       let chat_type = param.chat_type;
       query = { chat_type, receivers: { $all: [user_id] } };
-      let paginationData = await ChatList.paginate(query, { page, limit, sort: { createdAt: -1 }, lean: true });
-      let chatInfo: any = { chat_type, user_id: param.user_id, users: [] }
+      let paginationData = await ChatList.paginate(query, {
+        page,
+        limit,
+        sort: { createdAt: -1 },
+        lean: true,
+      });
+      let chatInfo: any = { chat_type, user_id: param.user_id, users: [] };
       //add last msg/chat in chatlist
       paginationData.docs = await Helper.addLastChatInList(paginationData.docs);
-      chatInfo.users = await Helper.addUserInfoInChatList(users_info_Url, paginationData.docs);
+      chatInfo.users = await Helper.addUserInfoInChatList(
+        users_info_Url,
+        paginationData.docs
+      );
       Helper.sendPaginationResponse(res, paginationData, chatInfo);
-
+    } catch (error) {
+      Helper.sendNotFoundResponse(
+        res,
+        param.chat_type == "user-group" ? "Group" : "Chat list"
+      );
     }
-    catch (error) {
-      Helper.sendNotFoundResponse(res, param.chat_type == 'user-group' ? 'Group' : 'Chat list');
-    }
-  }
-  else Helper.sendNotFoundResponse(res, param.chat_type == 'user-group' ? 'Group' : 'Chat list');
-
-
+  } else
+    Helper.sendNotFoundResponse(
+      res,
+      param.chat_type == "user-group" ? "Group" : "Chat list"
+    );
 });
 
 //Fetch Chat for user to multi-user and user to group
@@ -42,10 +53,13 @@ ChatListRoutes.route("/chat").get(async (req, res, next) => {
   let param = req.query;
   if (param.chat_list_id) {
     query = { chat_list_id: param.chat_list_id };
-    let paginationData = await Chat.paginate(query, { page, limit, sort: { createdAt: -1 } });
+    let paginationData = await Chat.paginate(query, {
+      page,
+      limit,
+      sort: { createdAt: -1 },
+    });
     Helper.sendPaginationResponse(res, paginationData);
-  }
-  else Helper.sendNotFoundResponse(res, 'message');
+  } else Helper.sendNotFoundResponse(res, "message");
 });
 
 //Fetch Chat for user to user
@@ -60,14 +74,15 @@ ChatListRoutes.route("/user-user-chat").get(async (req, res, next) => {
     try {
       query = Helper.userToUserQuery(sender, receiver);
       let chat_list = await ChatList.findOne(query);
-      let paginationData = await Chat.paginate({ chat_list_id: chat_list._id }, { page, limit, sort: { createdAt: -1 } });
+      let paginationData = await Chat.paginate(
+        { chat_list_id: chat_list._id },
+        { page, limit, sort: { createdAt: -1 } }
+      );
       Helper.sendPaginationResponse(res, paginationData, { sender, receiver });
+    } catch (error) {
+      Helper.sendNotFoundResponse(res, "message");
     }
-    catch (error) {
-      Helper.sendNotFoundResponse(res, 'message');
-    }
-  }
-  else Helper.sendNotFoundResponse(res, 'message');
+  } else Helper.sendNotFoundResponse(res, "message");
 });
 
 //Add Chat
@@ -81,9 +96,8 @@ ChatListRoutes.route("/chat").post(async (req, res, next) => {
       let chatList = await new Chat({ sender, chat_list_id, text });
       await chatList.save();
       Helper.sendResponse(res, chatList);
-    }
-    catch (e) {
-      Helper.errorResponse(res, 'Something went wrong.Please try again.');
+    } catch (e) {
+      Helper.errorResponse(res, "Something went wrong.Please try again.");
     }
   }
 });
@@ -93,41 +107,44 @@ ChatListRoutes.route("/add").post(async (req, res, next) => {
   let body = req.body;
   let receivers = body.receivers || [];
   if (body.created_by && body.receivers && body.chat_type) {
-
     let allIds = [...receivers, body.created_by];
-    let data = { chat_type: body.chat_type, created_by: body.created_by, receivers: allIds, name: '', image: '' };
-    if (body.chat_type == 'user-group') {
+    let data = {
+      chat_type: body.chat_type,
+      created_by: body.created_by,
+      receivers: allIds,
+      name: "",
+      image: "",
+    };
+    if (body.chat_type == "user-group") {
       data.name = body.name;
       data.image = body.image;
     }
     try {
       let chatList;
 
-      if (body.chat_type == 'user-user') {
+      if (body.chat_type == "user-user") {
         // Not add duplicate chat for user-user
-        let query = Helper.userToUserChatListQuery(body.created_by, body.receivers[0]);
+        let query = Helper.userToUserChatListQuery(
+          body.created_by,
+          body.receivers[0]
+        );
         let get_chatList = await ChatList.findOne(query);
         if (!get_chatList) {
-
           chatList = await new ChatList(data);
           await chatList.save();
-        }
-        else {
+        } else {
           chatList = get_chatList;
         }
-      }
-      else {
+      } else {
         chatList = new ChatList(data);
         chatList.save();
       }
       let getData = await ChatList.find({ _id: chatList._id });
       Helper.sendResponse(res, getData);
+    } catch (e) {
+      Helper.errorResponse(res, "Something went wrong.Please try again.");
     }
-    catch (e) {
-      Helper.errorResponse(res, 'Something went wrong.Please try again.');
-    }
-  }
-  else Helper.errorResponse(res, 'created_by or chat_type is missing/');
+  } else Helper.errorResponse(res, "created_by or chat_type is missing/");
 });
 
 //Update receivers in ChatList/Group
@@ -140,16 +157,12 @@ ChatListRoutes.route("/update").post(async (req, res, next) => {
         { _id: chat_list_id },
         { receivers: receivers }
       );
-      Helper.messageResponse(res, 'Receivers Updated successfully!');
+      Helper.messageResponse(res, "Receivers Updated successfully!");
+    } catch (e) {
+      Helper.errorResponse(res, "Something went wrong.Please try again.");
     }
-    catch (e) {
-      Helper.errorResponse(res, 'Something went wrong.Please try again.');
-    }
-  }
-  else Helper.errorResponse(res, 'Something went wrong.Please try again.');
+  } else Helper.errorResponse(res, "Something went wrong.Please try again.");
 });
-
-
 
 //Remove ChatList/Group
 ChatListRoutes.route("/remove").post(async (req, res, next) => {
@@ -159,15 +172,31 @@ ChatListRoutes.route("/remove").post(async (req, res, next) => {
       let query = { _id: ids };
       await ChatList.deleteMany(query);
       await Chat.deleteMany({ chat_list_id: { $in: ids } });
-      Helper.messageResponse(res, 'Removed successfully!');
+      Helper.messageResponse(res, "Removed successfully!");
+    } catch (e) {
+      Helper.errorResponse(res, "Something went wrong.Please try again.");
     }
-    catch (e) {
-      Helper.errorResponse(res, 'Something went wrong.Please try again.');
-    }
-  }
-  else Helper.errorResponse(res, 'ids missing.');
+  } else Helper.errorResponse(res, "ids missing.");
 });
 
+ChatListRoutes.route("/delete").delete(async (req, res, next) => {
+  const chatList = req.body.chatList;
+  if (chatList.length > 0) {
+    for (let i in chatList) {
+      let { chat_list_id, userId } = chatList[i];
+      try {
+        await ChatHistory.updateOne(
+          { chat_list_id, userId },
+          { $set: { chat_list_id, userId } },
+          { upsert: true }
+        );
+      } catch (e) {
+        Helper.errorResponse(res, "Something went wrong.Please try again.");
+      }
+    }
+    Helper.messageResponse(res, "Deleted successfully!");
+  } else Helper.errorResponse(res, "ids missing.");
+});
 
 //Fetch Chat for user to multi-user and user to group
 ChatListRoutes.route("/get_details").get(async (req, res, next) => {
@@ -180,11 +209,9 @@ ChatListRoutes.route("/get_details").get(async (req, res, next) => {
     query = { created_by, chat_type };
     if (group_name) query.name = group_name;
     let chat_list_details = await ChatList.findOne(query);
-    console.log("get_details ", chat_list_details)
+    console.log("get_details ", chat_list_details);
     Helper.sendResponse(res, chat_list_details);
-  }
-  else Helper.sendNotFoundResponse(res, 'created_by or chat_type missing.');
+  } else Helper.sendNotFoundResponse(res, "created_by or chat_type missing.");
 });
-
 
 export default ChatListRoutes;
